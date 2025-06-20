249c7c3a88a0b2c1a72360fb50db9a48be1a2244
John Storozuk <johnstorozuk@johns-mbp.lan>
2025-06-19 22:53:30 -0400

    feat: Display timestamps in user's local timezone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_time_zone
+
+  private
+
+  def set_time_zone
+    Time.zone = cookies[:timezone] if cookies[:timezone]
+  end
+
+  def current_user_has_time_zone?
+    cookies[:timezone].present?
+  end
 end
